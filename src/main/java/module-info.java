2f8c9158da183c8ec3e7d25468696521fd783493
QianMason <thunderpurtz@ubuntu>
2021-03-30 17:24:18 -0700

    updated module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,6 @@
+import org.homi.plugin.api.IPluginFactory;
+
 module org.homi.deviceRegistry {
 	requires org.homi.plugin.api;
-	
+	provides IPluginFactory with deviceRegistry.DeviceRegistryFactory;
 }
